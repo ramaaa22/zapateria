d9c7961a132f79eab6217b72f6ec526f712c2dab
ramaaa22 <ramiro.boza@hotmail.com>
2023-03-01 10:54:51 -0300

    changed validation in brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand < ApplicationRecord
     has_many :models
-    
-    validates :cod , presence: true , uniqueness: true, length: { is: 2 }
-    validates :name , presence: true, uniqueness: true, length: {minimum: 3, maximum: 10}
+
+    validates :cod , presence: true , uniqueness: true, length: { is: 2}, format: {with: /[a-zA-Z]/}
+    validates :name , presence: true, uniqueness: true, length: {minimum: 3, maximum: 20}
 end
